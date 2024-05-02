121f463ea33a6dbe38e3cf1b90da1d15bce710c3
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2024-05-02 13:03:58 -0500

    Agregando Rutas de likes faltantes y Readme actualizado

--- routes/cards.js
+++ routes/cards.js
@@ -8,5 +8,7 @@ const cardController = require('../controllers/card');
 router.get('/', cardController.getAllCards);
 router.post('/', cardController.createCard);
 router.delete('/:cardId', cardController.deleteCardById);
+router.put('/:cardId/likes', cardController.likeCard);
+router.delete('/:cardId/likes', cardController.dislikeCard);
 
 module.exports = router;
